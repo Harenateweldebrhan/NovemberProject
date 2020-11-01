a938af0c972460bdf9c3be82e3980807a27a9fa4
Harenateweldebrhan <Haru7095@gmail.com>
2020-11-01 11:23:36 -0500

    Batman has bat mobile

--- src/main/java/Batman.java
+++ src/main/java/Batman.java
@@ -2,5 +2,6 @@ public class Batman {
     public static void main(String[] args) {
         System.out.println("heyyyy");
         System.out.println("llllll");
+        //Batman has cool batmobile
     }
 }
